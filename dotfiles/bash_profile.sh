f90b42dc7ccf6aea451c5a0d88e70affce16c2ec
bcole <bcole@datapipe.com>
2016-12-30 16:21:04 -0500

    Purge docker images

--- dotfiles/bash_profile.sh
+++ dotfiles/bash_profile.sh
@@ -105,3 +105,12 @@ function docker_cleanup() {
     docker rmi $(docker images -a | grep "^<none>" | awk '{print $3}')
     docker images -q --filter "dangling=true" | xargs docker rmi
 }
+
+# Purges all versions of an image by name
+# e.g. docker_purge registry.com/imagename
+function docker_delete_image() {
+
+    local image=$1
+    docker images | grep $image | awk {'print $3'} | xargs docker rmi
+
+}
