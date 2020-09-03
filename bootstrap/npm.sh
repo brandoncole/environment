ffbc8852a52a8f23868a75786fa4cb55d1563d26
Brandon Cole <brandon.cole@candidpartners.com>
2020-09-03 13:54:05 -0400

    Add NPM modules

---
+++ bootstrap/npm.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+function prereq() {
+
+    which -s npm && return 0
+    return 1
+
+}
+
+function install() {
+
+    prereq
+
+    local modules=(
+        aws-cdk
+        yarn
+    )
+
+	for module in "${modules[@]}"; do
+        echo "Install ${module}? (Enter 'y' to install or any other key to skip)"
+        read -s -n 1 input
+        if [ $input == "y" ]; then
+            echo Installing ${module}...
+            npm install -g ${module}
+        else
+            echo Skipping ${module}
+        fi
+	done
+
+}
+
+install
